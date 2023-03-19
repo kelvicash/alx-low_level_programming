f7b8010497a376b4db68c0680d4a67cd3556496a
kelvicash <you@gmail.com>
2023-03-18 17:21:22 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ */
+int main()
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return(1);
+}
