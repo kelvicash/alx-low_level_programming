23a24c424351753a195ffc4d28cb730077b795b5
kelvicash <you@gmail.com>
2023-03-20 14:12:09 -0700

    advanced loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
