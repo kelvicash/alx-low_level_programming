f441d1099a7366f9dda085f9b892c4fb1b952d8d
kelvicash <you@gmail.com>
2023-03-20 15:03:12 -0700

    random numbers in c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,23 @@
 int main(void)
 {
 	int n;
+	int m;
 
-	for (n = 0; n <= 100; n++)
+	for (n = '0'; n < '9'; n++)
+	{
+	for (m = n + 1; m <= '9'; m++)
+	{
+	if (m != n)
 	{
 		putchar(n);
+		putchar(m);
+		if (n == '8' && m == '9')
+		continue;
+		putchar(',');
+		putchar(' ');
 	}
+	}
+	}
+	putchar('\n');
 	return (0);
 }
