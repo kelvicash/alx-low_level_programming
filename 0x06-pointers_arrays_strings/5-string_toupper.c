f9e512a918674ae965b08e97ddbe6576e32899c0
kelvicash <you@gmail.com>
2023-03-30 16:46:06 -0700

    pointers and arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - transform to upper
+ *
+ * @n: pointer
+ *
+ * Return: chsrscter trns
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++
+	}
+	return (n);
+}
